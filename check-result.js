0fe5712fc6d1d4634c9f81d668f1e44ea5b5ce50
Nathan Kruckenberg <natekruck1990@gmail.com>
2020-07-08 16:05:40 -0700

    added get-random-throw function

---
+++ check-result.js
@@ -0,0 +1,11 @@
+
+// rock = 1
+// paper = 2
+// scissors = 3
+
+export function checkResult(player, computer) {
+    if (player === computer);
+    return 0;
+} else if (player === 1 && computer === 2) {
+    return 2;
+}
---
+++ get-random-throw.js
@@ -0,0 +1,15 @@
+/* getRandomThrow function that generates a random number then returns rock, paper, scissors. */
+
+export function getRandomThrow() {
+    const randomResult = Math.ceil(Math.random() * 3);
+
+    if (randomResult === 0) {
+        return 'rock';
+    } else if (randomResult === 1) {
+        return 'paper';
+    } else if (randomResult === 2) {
+        return 'scissors';
+    }
+}
+
+
