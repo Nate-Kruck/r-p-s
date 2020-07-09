8fdd8f53e40d50081d44a2dec0e15a062720d4c8
Nathan Kruckenberg <natekruck1990@gmail.com>
2020-07-08 22:48:40 -0700

    was able to get event listener working properly

--- r-p-s.js
+++ r-p-s.js
@@ -1,40 +1,52 @@
 // import functions and grab DOM elements
-import checkResult from './check-result.js';
-import getRandomThrow from './get-random-throw.js';
+import { checkResult } from './check-result.js';
+import { getRandomThrow } from './get-random-throw.js';
 
-const button = document.querySelector('play-button');
-const winner = document.querySelector('wins');
-const loser = document.querySelector('losses');
-const draw = document.querySelector('draws');
-// const results = document.querySelector('results');
-const message = document.querySelector('user-message');
+
+const button = document.querySelector('#play-button');
+const winnerScreen = document.querySelector('#wins');
+const loserScreen = document.querySelector('#losses');
+const drawScreen = document.querySelector('#draws');
+const resultsScreen = document.querySelector('#results');
+const choiceScreen = document.querySelector('#choices');
+
 // initialize state
 let wins = 0;
-let losses = 0;
-let totalDraws = 0;
+let draws = 0;
+let choices = 0;
 
 // set event listeners to update state and DOM
 button.addEventListener('click', () => {
-    const game = checkResult();
+    choices++;
+    const computer = getRandomThrow();
+    const checkedRadio = document.querySelector('input:checked');
+    const playerGuess = checkedRadio.value;
 
-    const playButton = document.querySelector('input:checked');
+    const winDraw = wins + draws;
+    const lossData = choices - winDraw;
 
-    const won = playButton.value === game;
-    
+    const result = checkResult(playerGuess, computer);
 
-    if (won) {
-        message.textContent = 'Winner!';
+// conditional
+
+    if (result === 'win') {
         wins++;
-        winner.textContent = wins;
-    } else if (loss) {
-        message.textContent = 'Losser';
-        losses++;
-        loser.textContent = losses;
-    } else {
-        message.textContent = 'Draw!';
-        totalDraws++;
-        draw.textContent = totalDraws;
+        winnerScreen.textContent = 'You are the winner!';
     }
+    
+    if (result === 'draw') {
+        draws++;
+        drawScreen.textContent = 'You tied!';
+    }
+    
+    if (result === 'lose') {
+        loserScreen.textContent = 'You are the loser!';
+    }
+
+    choiceScreen.textContent = `Choices: ${choices}`;
+    winnerScreen.textContent = `Wins: ${wins}`;
+    loserScreen.textContent = `Losses: ${lossData}`;
+    resultsScreen.textContent = `Results: ${result}`;
 });
 
 
